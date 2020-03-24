35088c69fb825df6488e9768bbcc0bfa2a0ae39c
Vivian Losciale <vivian.losciale@etu.univ-cotedazur.fr>
2020-03-24 13:49:08 +0100

    ajout creation+reception quiz

---
+++ app/models/quiz.model.js
@@ -0,0 +1,13 @@
+const Joi = require('joi')
+const BaseModel = require('../utils/base-model.js')
+
+module.exports = new BaseModel('Quiz', {
+  id: Joi.string().required(),
+  questions: Joi.array().items(Joi.object({
+    label: Joi.string().required(),
+    answers: Joi.array().items(Joi.object({
+      value: Joi.string().required(),
+      isCorrect: Joi.boolean(),
+    })),
+  })),
+})
