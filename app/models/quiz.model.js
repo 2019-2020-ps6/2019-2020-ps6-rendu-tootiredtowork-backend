74951d4be2122fdcc0ebf9e2236e4f7a357ac06a
Vivian Losciale <vivian.losciale@etu.univ-cotedazur.fr>
2020-03-31 19:09:16 +0200

    liaison front

--- app/models/quiz.model.js
+++ app/models/quiz.model.js
@@ -3,6 +3,7 @@ const BaseModel = require('../utils/base-model.js')
 
 module.exports = new BaseModel('Quiz', {
   id: Joi.string().required(),
+  difficulty: Joi.number().integer(),
   questions: Joi.array().items(Joi.object({
     label: Joi.string().required(),
     answers: Joi.array().items(Joi.object({
